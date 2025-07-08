edc9f9cbc1427dfe9d4a4a939285152be43b804f
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-08 17:40:50 +0500

    URL Mapping

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import BookTitle, Book
 # Create your views here.
+def book_title_list_view(request):
+    list_of_book_titles = BookTitle.objects.all()
+    context = {
+        'list_of_book_titles': list_of_book_titles
+    }
+    return render(request, 'books/main.html', context)
+
+def book_title_detail_view(request, id):
+    book_detail = BookTitle.objects.get(pk=id)
+    context = {
+        'book_detail': book_detail
+    }
+    return render(request, 'books/detail.html', context)
+
