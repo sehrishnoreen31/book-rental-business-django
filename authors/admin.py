aafe20d2fa90fe6f72c322af76a6a089e486cc55
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-11 10:22:14 +0500

    Data export logic for authors

--- authors/admin.py
+++ authors/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Author
-# Register your models here.
-admin.site.register(Author)
+# import export
+from import_export import resources
+from import_export.admin import ExportActionMixin
+from import_export.fields import Field
+
+# resorces
+class AuthorResource(resources.ModelResource):
+    id = Field(attribute='id', column_name='ID')
+    name = Field(attribute='name', column_name='Name')
+    
+    class Meta:
+        model = Author
+        fields = ('id', 'name')
+        export_order = fields
+
+# register, admin
+@admin.register(Author)
+class AuthorAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = AuthorResource
